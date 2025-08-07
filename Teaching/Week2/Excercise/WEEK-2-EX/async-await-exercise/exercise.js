6d0d638b4bd83e22bfb258476f502043261ec808
visalxz69696969 <saovisal12192005@gmail.com>
2025-08-07 16:26:07 +0700

    add async await

---
+++ Teaching/Week2/Excercise/WEEK-2-EX/async-await-exercise/exercise.js
@@ -0,0 +1,24 @@
+// Exercise 2: Use async/await to get your bank account balance.
+// 1. Call the fetchBankAccountBalanceSimulated function using await.
+// 2. Store the result in a variable.
+// 3. Print: "Your bank account balance is <balance> USD" to the console.
+
+
+//! Don't forget to use await when calling the fetch function.
+
+//* This is a function to simulate fetching data which takes 2 seconds to resolve.
+//* Don't pay attention too much to this function, it is just a simulation
+const fetchBankAccountBalanceSimulated = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(1000);
+		}, 2000);
+	});
+};
+
+const run = async () => {
+	const balance = await fetchBankAccountBalanceSimulated();
+	console.log(`Your bank account balance is ${balance} USD`);
+};
+
+run();
